4eba8ac36ca536aea004f7e8f5b14217865f7fdf
beneditatan <benedita.tan@gmail.com>
2020-07-02 15:02:35 +0700

    added logger to unittest

--- index.js
+++ index.js
@@ -10,23 +10,9 @@ const jsonParser = bodyParser.json();
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 const { RideManager } = require('./src/models');
+const { logger } = require('./src/core');
 
 const buildSchemas = require('./src/schemas');
-
-const winston = require('winston');
- 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { 
-      server: 'localhost:8010',
-      service: 'ride-service' 
-    },
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
 
 db.serialize(() => {
     buildSchemas(db);
--- src/core/index.js
+++ src/core/index.js
@@ -2,10 +2,12 @@ const DBUtil = require('./utils/dbutil');
 const ObjectNotFound = require('./exceptions/objectnotfound');
 const ErrorCode = require('./enums/errorcode');
 const ValidationError = require('./exceptions/validationerror');
+const logger = require('./logger');
 
 module.exports = {
     DBUtil,
     ObjectNotFound,
     ValidationError,
-    ErrorCode
+    ErrorCode,
+    logger
 }
